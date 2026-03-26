a35a68c0ee705b24de724e8c5b68e71465b3193d
Lionel-Villanueva <salim.villanueva@tecsup.edu.pe>
2026-03-26 15:55:52 -0500

    Avance 1

--- Assets/Scripts/AgentSimpleController.cs
+++ Assets/Scripts/AgentSimpleController.cs
@@ -5,13 +5,11 @@ public class AgentSimpleController : MonoBehaviour
 {
     public Transform Target;
     private NavMeshAgent agent;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Target != null)
---
+++ Assets/Scripts/EnemyTerrestre.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyTerrestre : MonoBehaviour
+{
+    //Speed, Acceleration, AngularSpeed, StoppingDistance, AvoidancePriority
+    public float Speed;
+    public float Acceleration;
+    public float AngularSpeed;
+    public float StoppingDistance;
+    public float AvoidancePriority;
+
+
+
+
+
+}
